27b2f25c5319b56e8ba1dc010a825a41d8926fd4
Sebasti348 <valeriosebastian26@gmail.com>
2024-04-04 16:51:44 -0300

    TPN1PV_ValerioSebastian

--- src/ar/edu/unju/fi/Proyecto.java
+++ src/ar/edu/unju/fi/Proyecto.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi;
 
 public class Proyecto {
-
+	public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
 }
---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 5;
+        int num3 = 1;
+        int num4 = 20;
+        int num5 = 9;
+        
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+        
+        System.out.println("El promedio es: " + promedio);
+    }
+}
