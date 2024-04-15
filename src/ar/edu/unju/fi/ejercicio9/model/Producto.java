0c73b5a35b3ec665111a3ec51b55b57cb9e9c5be
Sebasti348 <valeriosebastian26@gmail.com>
2024-04-15 16:56:41 -0300

    Valerio Sebastian

---
+++ src/ar/edu/unju/fi/ejercicio9/model/Producto.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio9.model;
+
+public class Producto {
+    private String nombre;
+    private String codigo;
+    private double precio;
+    private int descuento;
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    public Producto(String nombre, String codigo, double precio, int descuento) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+        this.descuento = descuento;
+    }
+
+    public Producto() {
+    }
+
+    public double calcularDescuento() {
+        return this.precio - (this.precio * this.descuento / 100.0);
+    }
+}
